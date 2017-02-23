e207e9fa46e374498c54547f4ae898372670edcc
b3j0f <ib3j0f@gmail.com>
2017-02-23 05:25:02 +0100

    :remove: local file

--- www/web/settings/base.py
+++ www/web/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 import os
 
-from .private import SECRET_KEY, HOST, reCAPTCHA_SECRET_KEY
+from .local import SECRET_KEY, HOST, reCAPTCHA_SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(
